f1a4a06d15ee9ce94f5a65a2f4f7221c49183868
GuiBritx <guipinheiro2004@hotmail.com>
2021-03-18 12:29:56 +0000

    Aula 01 - Primeiro Componente
    Signed-off-by: GuiBritx <guipinheiro2004@hotmail.com>

---
+++ fundamentos01/src/components/basico/OlaMundo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default () => {
+    const informacao = 'Primeiro componente jsx'
+    return(
+        <>
+            <h2>Componente OlaMundo</h2>
+            <p>{ informacao }</p>
+        </>
+    )
+}
